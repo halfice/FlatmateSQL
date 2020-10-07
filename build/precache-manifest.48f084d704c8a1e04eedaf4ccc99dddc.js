0a946d7abf6c5dbc5a42232838e134996a18a6e4
Abdul Aziz Farooqi <axix.szabist@gmail.com>
2020-10-07 06:38:06 +0400

    sdoin

---
+++ build/precache-manifest.48f084d704c8a1e04eedaf4ccc99dddc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af69931ecb911fe7ba7bb2303519c3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c3442fd89d9819264ce",
+    "url": "/static/css/2.a8dd744c.chunk.css"
+  },
+  {
+    "revision": "a84dd9b1a952e22d2507",
+    "url": "/static/css/main.9cb69ed8.chunk.css"
+  },
+  {
+    "revision": "3c3442fd89d9819264ce",
+    "url": "/static/js/2.d7fd31e4.chunk.js"
+  },
+  {
+    "revision": "a84dd9b1a952e22d2507",
+    "url": "/static/js/main.bde2ae29.chunk.js"
+  },
+  {
+    "revision": "153d21d4fa94a0664d65",
+    "url": "/static/js/runtime~main.854a1653.js"
+  },
+  {
+    "revision": "6fdd7cf0ecfc339d68d07587ff931f22",
+    "url": "/static/media/images.6fdd7cf0.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6e51b1732529e8475735eccce2b1b1fd",
+    "url": "/static/media/mainicon.6e51b173.gif"
+  },
+  {
+    "revision": "61410e9c5c66ba51358bc498a76d25ad",
+    "url": "/static/media/uloaaderimage.61410e9c.gif"
+  }
+]);
