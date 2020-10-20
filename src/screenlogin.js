7364d05c092d16aca1cd51503dd0a6e762924485
Abdul Aziz Farooqi <axix.szabist@gmail.com>
2020-10-20 13:40:41 +0400

    binding functions

--- src/screenlogin.js
+++ src/screenlogin.js
@@ -59,7 +59,7 @@ export class screenlogin extends React.Component {
 
          var loginurl="https://userfunctionsapi.azurewebsites.net/api/HttpTriggerusers?code=zLwRL3jpIUtF0oWql4lfK38n/Ld6w5Ed6XzP1H7Kj3tBSF4dzL1crg==&email="+this.state.email+"&functiontype=c";
         axios
-         .post(loginurl)
+         .get(loginurl)
          .then(res => {
                 console.log(res);
                 _Response=res;
