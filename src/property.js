f7a42b9ae1410ce91cc5ad6c86f875f64921dd39
abdulaziz <axix.szabist@gmail.com>
2020-11-27 08:13:38 +0400

    binding functions

--- src/property.js
+++ src/property.js
@@ -210,8 +210,8 @@ this.getblobtoken();
       const account = "<account name>";
       const sas = this.state.blobtoken;
        //https://userfunctionsapi.azurewebsites.net/?st=2020-11-04T18%3A49%3A22Z&se=2020-11-04T19%3A49%3A22Z&sp=W&sv=2018-03-28&sr=b&sig=2tbOll2oU1JdvkxLiHui%2BpRU6nHqsA0uKNtDF%2BsfZQU%3D
-      console.log(sas.data.token);
-       const blobServiceClient = new BlobServiceClient(`https://userfunctionsapi.blob.core.windows.net?${sas}`);
+      var finalToken=sas.data.token;
+       const blobServiceClient = new BlobServiceClient(`https://userfunctionsapi.blob.core.windows.net?${finalToken}`);
       let i = 1;
       let iter = blobServiceClient.listContainers();
       let containerItem =  iter.next();
