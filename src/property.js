ee4ebb8fd51df6250414b396baeec80ed34a8e1f
abdulaziz <axix.szabist@gmail.com>
2020-11-27 07:18:54 +0400

    binding functions

--- src/property.js
+++ src/property.js
@@ -161,6 +161,7 @@ this.getblobtoken();
     var loginurl="https://userfunctionsapi.azurewebsites.net/api/HttpTriggerStorageToken?code=TqfhfkL7Vgn0x/H7JHdqZQXTCzQZSMvAVcmKk2teC3ZOgTVSN3QYaA==";
     try {
                let res=await  axios.post(loginurl);
+               console.log(res);
                this.setState({
                 blobtoken:res,
                 loader:false,
@@ -210,7 +211,8 @@ this.getblobtoken();
       const account = "<account name>";
       const sas = this.state.blobtoken;
        //https://userfunctionsapi.azurewebsites.net/?st=2020-11-04T18%3A49%3A22Z&se=2020-11-04T19%3A49%3A22Z&sp=W&sv=2018-03-28&sr=b&sig=2tbOll2oU1JdvkxLiHui%2BpRU6nHqsA0uKNtDF%2BsfZQU%3D
-      const blobServiceClient = new BlobServiceClient(`https://userfunctionsapi.blob.core.windows.net?${sas}`);
+      
+       const blobServiceClient = new BlobServiceClient(`https://userfunctionsapi.blob.core.windows.net?${sas}`);
       let i = 1;
       let iter = blobServiceClient.listContainers();
       let containerItem =  iter.next();
