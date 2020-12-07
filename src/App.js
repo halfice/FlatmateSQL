088219e1daeaf4aa09609810d8410aa166809a14
abdulaziz <axix.szabist@gmail.com>
2020-12-07 22:10:43 +0400

    binding functions

--- src/App.js
+++ src/App.js
@@ -74,7 +74,9 @@ class App extends Component {
 
         }
 
-        {this.state.needwizard == 0 &&
+        {this.state.needwizard == 0 && this.state.needwizard != 1000 &&
+        this.state.needwizard != 100 && this.state.needwizard != 1 &&
+        this.state.needwizard != 500 &&
           <div className="row">
             <MainBodyCards handler={this.handler} />
             </div>
