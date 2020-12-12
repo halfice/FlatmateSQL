01329477a6af4ba3d2eb7c0c7e1e5c38764f3744
abdulaziz <axix.szabist@gmail.com>
2020-12-12 14:28:49 +0400

    binding functions

--- src/bodycards.js
+++ src/bodycards.js
@@ -346,15 +346,7 @@ class bodycards extends Component {
       
         <div className="CarousalDivItem">
           <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={item.picstring
-              
-              }
-              
-
-              alt="First slide"
-            />
+            <img className="d-block w-100" src={item.picstring} alt="First slide"/>
             {console.log(item)}
             <Carousel.Caption>
               <div className="row bottomborder" >
