2834b3e8d36e9dd5b99f00cf1e434b0b6d5e174a
unknown <farhan_88@live.co.uk>
2020-12-19 11:47:04 -0800

    test

--- src/bodycards.js
+++ src/bodycards.js
@@ -360,49 +360,19 @@ class bodycards extends Component {
     var carousalitem = null;
     carousalitem = this.state.carousalObjectitem.map((item, i) => {
       return (
-        <div className="CarousalDivItem">
-          <div className="container-fluid">
-
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src={simgstr}
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src={simgstr1}
-                alt="Third slide"
-              />
-
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
-              />
-
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-
-
-          </div>
-
-        </div>
+        <div class="carousel-wrapper">
+        <Carousel>
+            <div>
+                <img src="https://react-responsive-carousel.js.org/assets/1.jpeg" />
+            </div>
+            <div>
+                <img src="https://react-responsive-carousel.js.org/assets/2.jpeg" />
+            </div>
+            <div>
+                <img src="https://react-responsive-carousel.js.org/assets/3.jpeg" />
+            </div>
+        </Carousel>
+    </div>
 
       );
 
