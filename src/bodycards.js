b871970623931d950bf896e5deb9ba0a0c6a65e5
abdulaziz <axix.szabist@gmail.com>
2020-12-29 15:21:59 +0400

    adding agent info

--- src/bodycards.js
+++ src/bodycards.js
@@ -267,6 +267,7 @@ class bodycards extends Component {
 
 
   formatMoney(num) {
+    num =parseInt(num);
     var p = num.toFixed(2).split(".");
     return "$" + p[0].split("").reverse().reduce(function(acc, num, i, orig) {
         return  num=="-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
