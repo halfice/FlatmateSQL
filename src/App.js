d59683b2fc76fb770857833e5351f6ca4c993251
abdulaziz <axix.szabist@gmail.com>
2021-01-23 18:03:17 +0400

    updating apps

--- src/App.js
+++ src/App.js
@@ -114,7 +114,7 @@ class App extends Component {
 
         }
 
-        {this.state.needwizard == 0 && this.state.needwizard != 1000 &&
+        {this.state.needwizard == 888888888880 && this.state.needwizard != 1000 &&
           this.state.needwizard != 100 && this.state.needwizard != 1 &&
           this.state.needwizard != 500 && this.state.needwizard != 5 &&
           this.state.needwizard != 600 &&
