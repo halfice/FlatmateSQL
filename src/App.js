c1acd0e833467053bf0684a40a1bba75bfa3c06d
abdulaziz <axix.szabist@gmail.com>
2021-01-23 18:28:47 +0400

    updating apps

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      needwizard: 0,
+      needwizard: 999,
       ownerwizard: 0,
       userid: 0,
       showcard: 1,
