924d3044fafe12426a55061e50f78e25153ddb7e
abdulaziz <axix.szabist@gmail.com>
2021-01-23 20:46:37 +0400

    updating apps

--- src/SearchProperty.js
+++ src/SearchProperty.js
@@ -258,8 +258,12 @@ class SearchProperty extends Component {
   }
 
   formatMoney(num) {
-    var p = num.toFixed(2).split(".");
-    return "$" + p[0].split("").reverse().reduce(function (acc, num, i, orig) {
+    var p ="";
+    if (num.indexOf(".")>-1){
+      p = num.toFixed(2).split(".");
+    }
+    
+    return "AED" + p[0].split("").reverse().reduce(function (acc, num, i, orig) {
       return num == "-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
     }, "") + "." + p[1];
   }
