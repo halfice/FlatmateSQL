f3be2f9f6c9b4315d0232d781a29811b0d3d5b1e
abdulaziz <axix.szabist@gmail.com>
2021-05-14 10:58:51 +0400

    add

--- src/bodycards.js
+++ src/bodycards.js
@@ -928,7 +928,7 @@ class bodycards extends Component {
 
                   <div className="col-sm-4 ">
                     <div className="row agentdivCss">
-                      <div className="col-sm-8">
+                      <div className="col-sm-9">
                         <div className="leftdiv"> Agent :  {this.state.AgentName}</div>
                         <div className="leftdiv">Company : {this.state.AgentComapny}</div>
                       </div>
