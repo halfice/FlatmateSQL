d7b4e94efc7f6ac38cce8a9b8504deaf4235a61e
abdulaziz <axix.szabist@gmail.com>
2021-05-21 14:47:55 +0400

    sdf

--- src/Finishdeals.js
+++ src/Finishdeals.js
@@ -82,7 +82,7 @@ class Finishdeals extends Component {
      finishdealclick() {
 
 
-        var regurl = `https://userfunctionsapi.azurewebsites.net/api/HttpTriggerFinishdeal?code=Jfa1Z7DWGy4a30z4gz16iWIzn5nTgCt3UFrOPFrQOYgCPeSIFR69pQ==
+        var regurl = `https://userfunctionsapi.azurewebsites.net/api/HttpTriggerFinishdeal?code=ndZMLkOqIA3xyjAAMfWKjQUwlJ0O58FJgBrq1t7GcZBKaqWjvifZSQ==
         &partyone=${this.state.partyone}&partytwo=${this.state.partytwo}&amount=${this.state.dealamount}&dealdate=${this.state.dealdate}
         &userid=${this.props.UserID}&companyid=${this.props.AgentComapny}&companypic=${this.props.AgentPic}&agentname=${this.props.AgentName}&propertyid=${this.props.selectedPropertyId}&functiontype=add`;
         try {
--- src/mypropertydeals.js
+++ src/mypropertydeals.js
@@ -60,7 +60,7 @@ class mypropertydeals extends Component {
     var TempCarousalData = [];
 
 
-        var regurl = `https://userfunctionsapi.azurewebsites.net/api/HttpTriggerFinishdeal?code=Jfa1Z7DWGy4a30z4gz16iWIzn5nTgCt3UFrOPFrQOYgCPeSIFR69pQ==
+        var regurl = `https://userfunctionsapi.azurewebsites.net/api/HttpTriggerFinishdeal?code=ndZMLkOqIA3xyjAAMfWKjQUwlJ0O58FJgBrq1t7GcZBKaqWjvifZSQ==
         &partyone=${this.state.partyone}&partytwo=${this.state.partytwo}&amount=${this.state.dealamount}&dealdate=${this.state.dealdate}
         &userid=${this.props.UserID}&companyid=${this.props.AgentComapny}&companypic=${this.props.AgentPic}&agentname=${this.props.AgentName}&functiontype=get`;
         try {
