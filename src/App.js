a77571e27a09b1b05400ec8229a8550e84ef0d5a
abdulaziz <axix.szabist@gmail.com>
2021-05-23 17:47:54 +0400

    sdf

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      needwizard: 10189789789,
+      needwizard: 10189089789789,
       ownerwizard: 0,
       userid: 0,
       showcard: 1,
