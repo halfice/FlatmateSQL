21a7447fbb1ea54f7b29c1e86d72e88273b474a2
abdulaziz <axix.szabist@gmail.com>
2021-05-25 22:14:46 +0400

    234

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      needwizard: 5,
+      needwizard: 58798789,
       ownerwizard: 0,
       userid: 0,
       showcard: 1,
