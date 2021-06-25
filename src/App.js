97f830b95f6a491f92c87a02794f956a8e1ccb8d
abdulaziz <axix.szabist@gmail.com>
2021-06-25 15:12:07 +0400

    sg

--- src/App.js
+++ src/App.js
@@ -682,7 +682,7 @@ class App extends Component {
     var TempDivCounter = 0;
     var retrueneddata = [];
     var TempCarousalData = [];
-    var loginurl = "https://userfunctionsapi.azurewebsites.net/api/HttpTriggerProperty?code=ir1wJ4Nz5UQTl5jHM4K1IjP7oCCt2oJqXDhtwOv9ryoPH2ZRhpxc6w==&email=" + this.state.LoginUserID + "&functiontype=search&PropertyType=" + PropertyType + "&Location=" + Location + " &NoOfBedRooms=" + this.state.NoOfBedRooms + " &Furnished=" + Furnished + "&PriceMin=" + this.state.PriceMin + "&PriceMax=" + this.state.PriceMax + "&Statec=" + Statec + " &Deal=" + this.state.Deal + "&Feet=" + Feet + " &SeachBox=" + SeachBox + "";
+    var loginurl = "https://userfunctionsapi.azurewebsites.net/api/HttpTriggerProperty?code=ir1wJ4Nz5UQTl5jHM4K1IjP7oCCt2oJqXDhtwOv9ryoPH2ZRhpxc6w==&email=" + this.state.LoginUserID + "&functiontype=search&PropertyType=" + PropertyType + "&Location=" + Location + " &NoOfBedRooms=" + this.state.NoOfBedRooms + " &Furnished=" + Furnished + "&PriceMin=" + this.state.PriceMin + "&PriceMax=" + this.state.PriceMax + "&Statec=" + Statec + " &Deal=" + this.state.Deal + "&Feet=" + Feet + " &SeachBox=" + SeachBox + "&Purpose="+purpose+ "";
     try {
       let res = await axios.post(loginurl);
       console.log(res);
